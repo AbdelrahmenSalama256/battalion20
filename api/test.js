0f97d06f2c8bc13d098550dc6ebf8577f868d61f
AbdelrahmenSalama256 <abdosalama25802030@gmail.com>
2026-06-26 19:08:02 +0300

    Remove api/test.js

--- api/test.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.statusCode = 200;
-  res.end(JSON.stringify({ method: req.method, path: req.url, ok: true }));
-};
